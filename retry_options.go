f65b2cedd3395bf45de6f4fe37b4a198a139a62d
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2024-09-08 22:20:04 +0200

    feat(backoff strategies): enable backoff strategies and backoff limit
    Signed-off-by: PabloSanchi <pablo.sanchi.herrera@gmail.com>

--- retry_options.go
+++ retry_options.go
@@ -13,12 +13,14 @@ type RetryIfFunc func(error) bool
 
 // RetryConfig contains configuration options for the retry mechanism.
 type RetryConfig struct {
-	retries   uint
-	backoff   time.Duration
-	maxJitter time.Duration
-	onRetry   OnRetryFunc
-	retryIf   RetryIfFunc
-	context   context.Context
+	retries         uint
+	backoff         time.Duration
+	backoffStrategy func(base time.Duration, n uint) time.Duration
+	backoffLimit    time.Duration // maximum backoff time allowed
+	maxJitter       time.Duration
+	onRetry         OnRetryFunc
+	retryIf         RetryIfFunc
+	context         context.Context
 }
 
 // RetryOption is a function type for modifying RetryConfig options.
@@ -27,11 +29,13 @@ type RetryOption func(*RetryConfig)
 // newDefaultRetryConfig creates a default RetryConfig with sensible defaults.
 func newDefaultRetryConfig() *RetryConfig {
 	return &RetryConfig{
-		retries:   3,
-		backoff:   1 * time.Second,
-		maxJitter: 0 * time.Second,                            // no jitter by default
-		onRetry:   func(n uint, err error) {},                 // no-op onRetry by default
-		retryIf:   func(err error) bool { return err != nil }, // retry on any error by default
-		context:   context.Background(),
+		retries:         3,
+		backoff:         1 * time.Second,
+		backoffStrategy: func(base time.Duration, n uint) time.Duration { return base },
+		backoffLimit:    0,                                          // no limit by default
+		maxJitter:       0,                                          // no jitter by default
+		onRetry:         func(n uint, err error) {},                 // no-op onRetry by default
+		retryIf:         func(err error) bool { return err != nil }, // retry on any error by default
+		context:         context.Background(),
 	}
 }
